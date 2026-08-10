b0e93714343bb98147a786e076ba467b3d545cbe
Mohd Saqlain Kazi <99112381+scanlain@users.noreply.github.com>
2026-08-10 21:32:17 +0530

    placed the theme-toggle button inside the card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
-import { FaMoon, FaSun } from 'react-icons/fa6';
 
 function App() {
 
-  const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
-
-  useEffect(() => {
-
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-  }, [theme])
-
-  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+  
 
   return (
     <>
-      <button
-        className='theme-toggle'
-        onClick={toggleTheme}
-      >
-        {theme === 'light' ? <FaMoon/> : <FaSun/>}
-      </button>
+      
       <Card/>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,28 @@ import Header from "./Header"
 import '../App.css'
 import MainContent from "./MainContent"
 import ViewResume from "./ViewResume"
+import { useEffect, useState } from 'react'
+import { FaMoon, FaSun } from 'react-icons/fa6';
 
 const Card = () => {
+  const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme])
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
   return (
     <div className="card">
+      <button
+        className='theme-toggle'
+        onClick={toggleTheme}
+      >
+        {theme === 'light' ? <FaMoon/> : <FaSun/>}
+      </button>
         <Header/>
         <MainContent/>
         <ViewResume/>
